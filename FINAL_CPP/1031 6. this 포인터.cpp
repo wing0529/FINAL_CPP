265f31902da2a831fb0643126a5298ce802d9902
[nanana] <[100232@naver.com]>
2024-10-31 17:29:10 +0900

    무슨말인지 모르겠어요

--- FINAL_CPP/1031 6. this 포인터.cpp
+++ FINAL_CPP/1031 6. this 포인터.cpp
@@ -14,3 +14,41 @@ public:
 	int getWidth() { return width; }
 };
 // 실제 회사에서 이렇게 쓰는 사람은 없음
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+class Circle {
+public:
+	int x, y, radius;	// 원의 중심점과 반지름
+	string color;		// 원의 색상
+	void draw();
+};
+
+void Circle::draw()
+{					// 원을 화면에 그리는 함수
+	HDC hdc = GetWindowDC(GetForegroundWindow());
+	Ellipse(hdc, x - radius, y - radius, x + radius, y + radius);
+}
+int main()
+{
+	int n;
+	Circle* p;
+
+	cout << "몇 개의 원을 만들까요: ";
+	cin >> n;
+	p = new Circle[n];
+	for (int i = 0; i < n; i++) {
+		p[i].x = 100 + rand() % 300;
+		p[i].y = 100 + rand() % 200;
+		p[i].radius = rand() % 100;
+		p[i].draw();
+	}
+	delete[] p;
+	getch();
+	return 0;
+}
