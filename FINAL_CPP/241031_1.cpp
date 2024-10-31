2c85074cbcee6b4cd404843c03e5704ffa53cf30
[nanana] <[100232@naver.com]>
2024-10-31 16:44:16 +0900

    1031 shared ptr

---
+++ FINAL_CPP/241031_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//스마트 포인터
+	// auto_ptr
+	// unique_ptr 함수 내에서 사용
+	// shared_ptr 함수 전달, 카피
+	// Heap 
+
+		shared_ptr<int> p1 = make_shared<int>(5); // better
+		cout << p1.use_count() << endl;
+
+		shared_ptr<int> p2 = p1; // OK! (reference count)
+		cout << p2.use_count() << endl;
+
+		return 0;
+	
+		// when reference count becomes zero, the memory is freed.
+
+	// weak_ptr
+	
+
+}
